d24b6d260cd551584a717499cbb2bb3e481167ff
Stephane P. Pericat <contact@pericat.net>
2025-06-04 13:03:51 -0500

    feat: web search

--- app/(routes)/search/[searchId]/_components/display-result.tsx
+++ app/(routes)/search/[searchId]/_components/display-result.tsx
@@ -1,8 +1,36 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { TabList } from './tab-list'
+import { Answer } from './answer'
 
 export function DisplayResult({ record }: { record?: Record<string, string> }) {
   const [activeTab, setActiveTab] = useState('Answer')
+
+  const getApiSearchResults = async (record: Record<string, string>) => {
+    try {
+      const res = await fetch('/api/brave-search', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          searchInput: record?.search_input,
+          searchType: record?.type,
+        }),
+      })
+
+      return await res.json()
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  useEffect(() => {
+    if (record) {
+      getApiSearchResults(record!).then((res) => {
+        console.log(res)
+      })
+    }
+  }, [record])
 
   return (
     <div className="mt-7">
@@ -10,6 +38,7 @@ export function DisplayResult({ record }: { record?: Record<string, string> }) {
         {record?.search_input}
       </h2>
       <TabList activeTab={activeTab} setActiveTab={setActiveTab} />
+      {activeTab === 'Answer' && <Answer />}
     </div>
   )
 }
---
+++ app/api/brave-search/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from 'next/server'
+
+export async function POST(req: Request) {
+  const { searchInput, searchType } = await req.json()
+
+  console.log(searchInput, searchType)
+
+  if (!searchInput || !searchType) {
+    return NextResponse.json({ error: 'Missing search input or search type' })
+  }
+
+  try {
+    const search = await fetch(
+      `https://api.search.brave.com/res/v1/web/search?q=${encodeURIComponent(searchInput.trim())}`,
+      {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Accept-Encoding': 'gzip',
+          'X-Subscription-Token': process.env.BRAVE_SEARCH_API_KEY ?? '',
+        },
+      },
+    )
+
+    const result = await search.json()
+
+    return NextResponse.json(result)
+  } catch (e) {
+    console.error(e)
+    return NextResponse.json({ error: 'Something went wrong' })
+  }
+}
